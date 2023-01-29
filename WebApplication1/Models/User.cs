5acd6f54d1602f64bbb769a960ee8f4bdea24e3a
seanlam8 <imseanlam@gmail.com>
2023-01-28 21:10:07 -0800

    first commit

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; } = string.Empty;
+}
